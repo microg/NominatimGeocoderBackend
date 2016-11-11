60f4fe1a069939e54651ecfd65275cba409034f7
daktak <daktak@domo.ddns.net>
2016-11-11 15:38:42 +1100

    Create Android.mk

---
+++ Android.mk
@@ -0,0 +1,26 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := Nominatem
+LOCAL_MODULE_TAGS := optional
+LOCAL_PACKAGE_NAME := Nominatem
+
+nominatem_root  := $(LOCAL_PATH)
+nominatem_out   := $(OUT_DIR)/target/common/obj/APPS/$(LOCAL_MODULE)_intermediates
+nominatem_build := $(nominatem_root)/build
+nominatem_apk   := build/outputs/apk/NominatemBackend-release-unsigned.apk
+
+$(nominatem_root)/$(nominatem_apk):
+        rm -Rf $(nominatem_build)
+        mkdir -p $(nominatem_out)
+        mkdir -p $(nominatem_build)
+        ln -sf $(nominatem_out) $(nominatem_build)
+        cd $(nominatem_root) && JAVA_TOOL_OPTIONS="$(JAVA_TOOL_OPTIONS) -Dfile.encoding=UTF8" ./gradlew assembleRelease
+
+LOCAL_CERTIFICATE := platform
+LOCAL_SRC_FILES := $(nominatem_apk)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+
+include $(BUILD_PREBUILT)
