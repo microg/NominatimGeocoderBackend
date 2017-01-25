8aa65319555dcda61283209080f51de9a7d45bdd
Benoît Mauduit <bmauduit@beneth.fr>
2017-01-25 13:05:27 +0100

    Fix Android.mk
    Change-Id: I34d4e02cb768113b79261153931c2e72976fea1a

--- Android.mk
+++ Android.mk
@@ -9,14 +9,14 @@ LOCAL_PACKAGE_NAME := Nominatem
 nominatem_root  := $(LOCAL_PATH)
 nominatem_out   := $(OUT_DIR)/target/common/obj/APPS/$(LOCAL_MODULE)_intermediates
 nominatem_build := $(nominatem_root)/build
-nominatem_apk   := build/outputs/apk/NominatemBackend-release-unsigned.apk
+nominatem_apk   := build/outputs/apk/Nominatem-release-unsigned.apk
 
 $(nominatem_root)/$(nominatem_apk):
-        rm -Rf $(nominatem_build)
-        mkdir -p $(nominatem_out)
-        mkdir -p $(nominatem_build)
-        ln -sf $(nominatem_out) $(nominatem_build)
-        cd $(nominatem_root) && JAVA_TOOL_OPTIONS="$(JAVA_TOOL_OPTIONS) -Dfile.encoding=UTF8" ./gradlew assembleRelease
+	rm -Rf $(nominatem_build)
+	mkdir -p $(nominatem_out)
+	mkdir -p $(nominatem_build)
+	ln -sf $(nominatem_out) $(nominatem_build)
+	cd $(nominatem_root) && JAVA_TOOL_OPTIONS="$(JAVA_TOOL_OPTIONS) -Dfile.encoding=UTF8" ./gradlew assembleRelease
 
 LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := $(nominatem_apk)
