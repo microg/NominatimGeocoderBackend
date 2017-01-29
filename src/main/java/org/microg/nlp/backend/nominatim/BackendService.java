12135b928aa423cafd517ca2287651b967d32d26
Benoît Mauduit <bmauduit@beneth.fr>
2017-01-29 15:15:54 +0100

    Revert "Change to HttpsURLConnection"
    This reverts commit a156299f76cbf973ec8d8483784228330a83607a.

--- src/main/java/org/microg/nlp/backend/nominatim/BackendService.java
+++ src/main/java/org/microg/nlp/backend/nominatim/BackendService.java
@@ -17,6 +17,7 @@ import org.microg.nlp.api.GeocoderBackendService;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
@@ -24,8 +25,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class BackendService extends GeocoderBackendService {
     private static final String TAG = "NominatimGeocoder";
@@ -218,7 +217,7 @@ public class BackendService extends GeocoderBackendService {
             synchronized (done) {
                 try {
                     Log.d(TAG, "Requesting " + url);
-                    HttpsURLConnection connection = (HttpsURLConnection) new URL(url)
+                    HttpURLConnection connection = (HttpURLConnection) new URL(url)
                             .openConnection();
                     setUserAgentOnConnection(connection);
                     connection.setDoInput(true);
